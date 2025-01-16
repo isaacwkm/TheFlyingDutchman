aaa786cceb713a77b7a5b05ea1d8da07c21aed43
isaacwkm <61212700+isaacwkm@users.noreply.github.com>
2025-01-16 06:40:52 -0800

    fixed error

--- Assets/Scripts/Player Character Movement.cs
+++ Assets/Scripts/Player Character Movement.cs
@@ -7,7 +7,7 @@ public class PlayerCharacterMovement : MonoBehaviour
 {
     public Camera playerCamera;
     [SerializeField] private float walkSpeed = 6f;
-    [SerializeField] private float runSpeed = 12f;
+    //[SerializeField] private float runSpeed = 12f;
     [SerializeField] private float jumpPower = 7f;
     [SerializeField] private float gravity = 10f;
     [SerializeField] private float lookSpeed = 2f;
