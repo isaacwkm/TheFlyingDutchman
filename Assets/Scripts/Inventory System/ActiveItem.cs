788a9d7d8d76f76e5ddbbfc91d09ccd690766d84
isaacwkm <61212700+isaacwkm@users.noreply.github.com>
2025-02-20 20:11:42 -0800

    Update ActiveItem.cs

--- Assets/Scripts/Inventory System/ActiveItem.cs
+++ Assets/Scripts/Inventory System/ActiveItem.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(DroppedItem))]
 public class ActiveItem : MonoBehaviour
 {
-    [Tooltip("Do not re-assign during runtime.")] 
+    [Tooltip("Do not re-assign during runtime.")]
     public int itemIDPleaseDoNotChange = 1; // Do not re-assign during runtime. The weird variable naming is only to discourage ID re-assignment after it has been correctly set to the right item id.
     public Vector3 heldPositionOffset;
     public Vector3 heldRotationOffset;
@@ -12,8 +12,23 @@ public class ActiveItem : MonoBehaviour
     public string attackAnimName;
     private Quaternion defaultRotation;
 
-    void Awake(){
+    void Awake()
+    {
         defaultRotation = gameObject.GetComponent<DroppedItem>().defaultRotation;
+    }
+
+    void OnEnable()
+    {
+        
+    }
+
+    void OnDisable()
+    {
+        
+    }
+    void setPlayerHandAnim()
+    {
+        //a
     }
     public void SnapToHand(Transform item, Transform handPosition)
     {
@@ -26,31 +41,31 @@ public class ActiveItem : MonoBehaviour
         item.localRotation = defaultRotation * Quaternion.Euler(heldRotationOffset);
     }
 
-    public void doAttack(bool forcePlayAnim = false){
+    public void doAttack(bool forcePlayAnim = false)
+    {
         if (!hasAttack) return;
 
-        if (forcePlayAnim == true){
+        if (forcePlayAnim == true)
+        {
             handAnim.Play(attackAnimName, -1, 0);
         }
 
-        if (handAnim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !handAnim.IsInTransition(0)){
+        if (handAnim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1 && !handAnim.IsInTransition(0))
+        {
             handAnim.Play(attackAnimName, -1, 0);
         }
     }
 
-    void OnEnable(){
-    }
 
-    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
