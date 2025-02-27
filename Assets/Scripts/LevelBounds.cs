fc0a0e4033a66fb8f39db39f3225ca6b86bb40d0
isaacwkm <61212700+isaacwkm@users.noreply.github.com>
2025-02-26 16:14:22 -0800

    pushing level boundary change

---
+++ Assets/Scripts/LevelBounds.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class LevelBounds : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit(GameObject player)
+    {
+        //respawn here
+    }
+}
