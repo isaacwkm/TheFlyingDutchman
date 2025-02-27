77f62b092d186b41673f4425e91cf68f6df453b8
blujai831 <webmaster@blujai831.dev>
2025-02-26 22:58:03 -0800

    Make ship decelerate and stabilize more aggressively when anchor is lowered

--- Assets/Scripts/Flying Ship System/Anchor Control.cs
+++ Assets/Scripts/Flying Ship System/Anchor Control.cs
@@ -7,6 +7,9 @@ public class AnchorControl : MonoBehaviour
     [SerializeField] private float anchorMaxDropDistance = 30.0f;
     [SerializeField] private HingeJoint controlWheelHingeJoint;
     [SerializeField] private float controlWheelSpinFactor = 10.0f;
+    [SerializeField] private Rigidbody shipPhysicsController;
+    [SerializeField] private float shipDecelerationWhenAnchored = 0.05f;
+    [SerializeField] private float shipStabilizationWhenAnchored = 120.0f;
     private bool extended = false;
     private Rigidbody anchorRbody;
 
@@ -46,5 +49,23 @@ public class AnchorControl : MonoBehaviour
             Vector3.Project(anchorRbody.linearVelocity, transform.up).magnitude *
             Mathf.Sign(Vector3.Dot(anchorRbody.linearVelocity, transform.up));
         controlWheelHingeJoint.motor = motor;
+        if (extended)
+        {
+            // if the anchor is out, make the ship behave itself
+            shipPhysicsController.linearVelocity =
+                Vector3.Lerp(
+                    shipPhysicsController.linearVelocity,
+                    Vector3.zero,
+                    1.0f - Mathf.Pow(
+                        1.0f - shipDecelerationWhenAnchored,
+                        Time.deltaTime * 60.0f
+                    )
+                );
+            shipPhysicsController.AddTorque(
+                shipPhysicsController.mass*shipStabilizationWhenAnchored*Time.deltaTime *
+                    Vector3.Cross(shipPhysicsController.transform.up, Vector3.up),
+                ForceMode.Impulse
+            );
+        }
     }
 }
