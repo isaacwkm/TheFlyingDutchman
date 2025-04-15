378460d55339183fb313a2fb620dccdc87e3d5c0
sushisuwa <70131041+sushisuwa@users.noreply.github.com>
2025-04-15 13:15:14 -0700

    Working on Title Screen

--- Assets/Scripts/Level/OnGameStart.cs
+++ Assets/Scripts/Level/OnGameStart.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class OnGameStart : MonoBehaviour
 {
+
+    public InputModeManager inputSwitcher; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        inputSwitcher.SwitchToUIControls();
     }
 
     // Update is called once per frame
--- Assets/Scripts/Persistent Data/ProgressPref.cs
+++ Assets/Scripts/Persistent Data/ProgressPref.cs
@@ -6,7 +6,7 @@ public class ProgressPref : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        LoadPref();
+        //LoadPref();
     }
 
     public static int progress
