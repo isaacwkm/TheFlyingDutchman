f0abd0be894cc2a6581d541f35c9c56002f581b7
LeCodingOwl <nguyenjimmy627@gmail.com>
2025-04-21 15:53:55 -0700

    Jimmy's Contract Work
    Added interaction with pickup-able resources that update resource counter

---
+++ Assets/ResourceInteraction.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using static UnityEditor.Progress;
+
+public class ResourceInteraction : MonoBehaviour
+{
+
+    int gold = 0;
+    int log = 0;
+    private void OnTriggerEnter(Collider other)
+    {
+        DroppedItem droppedItem = other.GetComponent<DroppedItem>();
+        if (droppedItem.itemID == 6)
+        {
+            //Store chest here
+            gold = gold + 100;
+            Debug.Log("Gold Amount: " +  gold);
+            Destroy(other.gameObject);
+        }
+        else if (droppedItem.itemID == 7)
+        {
+            //Store log
+            log = log + 10;
+            Debug.Log("Log Amount: " + log);
+            Destroy(other.gameObject);
+        }
+    }
+}
