c58976b2e6c0036d1fd42449df39d9f72d89a6e5
sushisuwa <70131041+sushisuwa@users.noreply.github.com>
2025-04-23 23:51:27 -0700

    Adding Title Screen
    in world title screen added
    TODO: find better buttons and highlighted colors. Create a game logo

---
+++ Assets/Scripts/DevTools/QuitScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class QuitScript : MonoBehaviour
+{
+    // This method will be linked to the Button's OnClick event
+    public void Quit()
+    {
+        Misc.Quit();
+    }
+}
