0384efc75d36551ae56f1dff0dc2a63c2b47316f
sushisuwa <70131041+sushisuwa@users.noreply.github.com>
2025-05-15 03:42:55 -0700

    Credits Adjustments
    - Gave credits button on main menu same icon as other buttons on screen
    - Added a new back button on the credits screen
    - Adjusted sections of the Title Screen to get rid of decimals

--- Assets/Scripts/Level/OnGameStart.cs
+++ Assets/Scripts/Level/OnGameStart.cs
@@ -5,6 +5,7 @@ public class OnGameStart : MonoBehaviour
 {
     public Transform refPoint;
     public GameObject CreditUI;
+    public GameObject MapToolTip;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +13,7 @@ public class OnGameStart : MonoBehaviour
         cinematics.SetHUDActive(false);
         CreditUI.SetActive(false); // Activate the credit UI
         InputModeManager.Instance.SwitchToUIControls(); // Switch to Player Controls when the game starts
+        MapToolTip.SetActive(false); // Deactivate the map tooltip
     }
 
     // Update is called once per frame
